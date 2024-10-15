74821d2abb84cb1ad8c8454c413554ce7eb624a0
Brian W. Wolter <brianwolter@gmail.com>
2024-10-15 09:56:39 -0700

    ...

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,16 @@ pub struct Options {
   pub debug: bool,
   #[clap(long, help="Enable verbose output")]
   pub verbose: bool,
+  #[clap(long="client:id", help="The OAuth2 consumer client identifier")]
+  pub client_id: String,
+  #[clap(long="client:secret", help="The OAuth2 consumer client secret")]
+  pub client_secret: String,
+  #[clap(long="url:auth", help="The OAuth2 service authorization URL")]
+  pub auth_url: String,
+  #[clap(long="url:token", help="The OAuth2 service token URL")]
+  pub token_url: String,
+  #[clap(long="url:return", help="The OAuth2 consumer callback URL")]
+  pub return_url: String,
 }
 
 #[tokio::main]
@@ -28,6 +38,8 @@ async fn main() {
 }
 
 async fn cmd() -> Result<i32, error::Error> {
+  let opts = Options::parse();
+  println!("HI {} {}", &opts.auth_url, &opts.token_url);
   let birthday = warp::path!("birthday")
     .map(|| {
       "Ok"
